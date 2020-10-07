008aafc0b160121a5a4960797271ebe3be1e22c1
Marcone Freitas <marcfreitas0@gmail.com>
2020-10-07 16:09:35 -0300

    Primeiro usuario

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course;
 
+import com.educandoweb.course.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CourseApplication.class, args);
+
+
 	}
 
 }
